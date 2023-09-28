2a13416ff5891be525242c9b11f8eade1dc88173
Michaela202 <veselovskamichaela222@gmail.com>
2023-09-28 15:27:15 +0200

    I can add a user

---
+++ Application/Logic/UserLogic.cs
@@ -0,0 +1,49 @@
+﻿using Application.DaoInterfaces;
+using Application.LogicInterfaces;
+using Domain.DTOs;
+using Domain.Models;
+
+namespace Application.Logic;
+
+public class UserLogic : IUserLogic
+{
+    private readonly IUserDao userDao;
+
+    public UserLogic(IUserDao userDao)
+    {
+        this.userDao = userDao;
+    }
+    public async Task<User> CreateAsync(UserCreationDto dto)
+    {
+        User? existing = await userDao.GetByUsernameAsync(dto.UserName);
+        if (existing != null)
+            throw new Exception("Username already taken!");
+
+        ValidateData(dto);
+        User toCreate = new User
+        {
+            UserName = dto.UserName,
+            Password =dto.Password
+        };
+    
+        User created = await userDao.CreateAsync(toCreate);
+    
+        return created;
+    }
+    private static void ValidateData(UserCreationDto userToCreate)
+    {
+        string userName = userToCreate.UserName;
+        string password = userToCreate.Password;
+
+        if (userName.Length < 3)
+            throw new Exception("Username must be at least 3 characters!");
+
+        if (userName.Length > 15)
+            throw new Exception("Username must be less than 16 characters!");
+        if (password.Length < 8)
+            throw new Exception("Password must be at least 8 characters long");
+        if (password.Length > 40)
+            throw new Exception("Password must be maximum 14 characters");
+    }
+
+}
--- Domain/Models/Post.cs
+++ Domain/Models/Post.cs
@@ -2,5 +2,15 @@
 
 public class Post
 {
-    
+    public int Id { get; set; }
+    public User Owner { get; }
+    public string Title { get; }
+    public string Text { get; set; }
+
+    public Post(User owner, string title, string text)
+    {
+        Owner = owner;
+        Title = title;
+        Text = text;
+    }
 }
